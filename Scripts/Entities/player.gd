31457571668a0983b52f7afdfba1805594bb7300
FiFolker <elouanle@gmail.com>
2024-07-15 18:36:58 +0200

    door added

--- Scripts/Entities/player.gd
+++ Scripts/Entities/player.gd
@@ -42,7 +42,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack"):
 		if ui_script.is_shooting:
 			shoot()
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("debug"):
 		$HealthModule.health -= 5
 
 func shoot() -> void:
---
+++ Scripts/Environment/door.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+var can_open_door:bool = false
+@onready var animation_player = $AnimationPlayer
+@onready var timer = $Timer
+@onready var animated_sprite_3d = %AnimatedSprite3D
+
+func _ready()->void:
+	animated_sprite_3d.play("closed")
+	
+func _input(event) -> void:
+	if event.is_action_pressed("use") && can_open_door:
+		open_door()
+
+func open_door() -> void:
+	print("openning door ...")
+	animated_sprite_3d.play("opened")
+	animation_player.play("door_opening")
+	timer.start()
+
+func close_door() -> void:
+	print("closing door ...")
+	animated_sprite_3d.play("closed")
+	animation_player.play_backwards("door_opening")
+
+func _on_area_3d_body_entered(body) -> void:
+	can_open_door = true
+
+
+func _on_area_3d_body_exited(body) -> void:
+	can_open_door = false
+
+func _on_timer_timeout() -> void:
+	close_door()
+	
